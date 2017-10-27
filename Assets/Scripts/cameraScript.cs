980e9f65d88e081ffe8cd73352c1bc74c0b501f2
みょんた <>
2017-10-27 14:33:04 +0900

    もろもろ追加

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+
+    GameObject Mcamera;
+    GameObject Vrcamera;
+
+    Quaternion Mcamq;
+    Quaternion Vcamq;
+
+    // Use this for initialization
+    void Start()
+    {
+        Mcamera = GameObject.Find("CarWaypointBased");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mcamera.transform.rotation = Mcamq;
+        Vcamq = this.transform.rotation;
+        Vcamq = Mcamq;
+
+    }
+}
